9d3b726193d751e6a05592d85f63b11ec8338c1e
smd <smd@fct.unl.pt>
2021-03-26 23:58:21 +0000

    Completed Spreadsheet class...

--- SD2021-TP1-API/src/tp1/api/Spreadsheet.java
+++ SD2021-TP1-API/src/tp1/api/Spreadsheet.java
@@ -4,13 +4,95 @@ import java.util.Set;
 import java.util.List;
 
 public class Spreadsheet {
-	String sheetId;
-	String sheetURL;
-
-	String owner;
-	Set<String> sharedWith;
-
-	int lines, columns;
+	private String sheetId;
+	private String owner;
+	private String sheetURL;
+	private int lines, columns;
+	private Set<String> sharedWith;
+	private List<List<String>> rawValues;
 	
-	List<List<String>> rawValues;
+	public Spreadsheet() {	
+	}
+
+	public Spreadsheet(String sheetId, String owner, String sheetURL, int lines, int columns, Set<String> sharedWith,
+			List<List<String>> rawValues) {
+		super();
+		this.sheetId = sheetId;
+		this.owner = owner;
+		this.sheetURL = sheetURL;
+		this.lines = lines;
+		this.columns = columns;
+		this.sharedWith = sharedWith;
+		this.rawValues = rawValues;
+	}
+
+	public String getSheetId() {
+		return sheetId;
+	}
+
+
+	public void setSheetId(String sheetId) {
+		this.sheetId = sheetId;
+	}
+
+
+	public String getOwner() {
+		return owner;
+	}
+
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+
+	public String getSheetURL() {
+		return sheetURL;
+	}
+
+
+	public void setSheetURL(String sheetURL) {
+		this.sheetURL = sheetURL;
+	}
+
+
+	public int getLines() {
+		return lines;
+	}
+
+
+	public void setLines(int lines) {
+		this.lines = lines;
+	}
+
+
+	public int getColumns() {
+		return columns;
+	}
+
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+
+	public Set<String> getSharedWith() {
+		return sharedWith;
+	}
+
+
+	public void setSharedWith(Set<String> sharedWith) {
+		this.sharedWith = sharedWith;
+	}
+
+
+	public List<List<String>> getRawValues() {
+		return rawValues;
+	}
+
+
+	public void setRawValues(List<List<String>> rawValues) {
+		this.rawValues = rawValues;
+	}
+
 }
