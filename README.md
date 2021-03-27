6583402d59bda9adafa934990e5ce757066e93cc
Nuno Preguiça <nuno.preguica@fct.unl.pt>
2021-03-27 00:56:51 +0000

    version 0.9

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# sd2021-tp1-api
+# API for SD project 2020/21
+Web page: [https://preguica.github.io/sd2021/praticas2021/trab1/trab1](https://preguica.github.io/sd2021/praticas2021/trab1/trab1)
+
+**Do not forget to replace the student numbers in the pom.xml**
+
+**Version:** 0.9 - 26/mar - 23:00
+
+
---
+++ sd2021-tp1-api/src/tp1/api/service/soap/SoapUsers.java
@@ -0,0 +1,58 @@
+package tp1.api.service.soap;
+
+import java.util.List;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+import tp1.api.User;
+import tp1.api.service.java.Result;
+
+@WebService(serviceName=SoapUsers.NAME, targetNamespace=SoapUsers.NAMESPACE, endpointInterface=SoapUsers.INTERFACE)
+public interface SoapUsers {
+
+	static final String NAME = "users";
+	static final String NAMESPACE = "http://sd2021";
+	static final String INTERFACE = "tp1.api.service.soap.SoapUsers";
+
+	@WebMethod
+	String createUser(User user) throws UsersException;
+	
+	/**
+	 * Obtains the information on the user identified by name.
+	 * @param userId the userId of the user
+	 * @param password password of the user
+	 * @throws UsersException otherwise
+	 */
+	@WebMethod
+	Result<User> getUser(String userId, String password) throws UsersException;
+	
+	/**
+	 * Modifies the information of a user. Values of null in any field of the user will be 
+	 * considered as if the the fields is not to be modified (the id cannot be modified).
+	 * @param userId the userId of the user
+	 * @param password password of the user
+	 * @param user Updated information
+	 * @throws UsersException otherwise
+	 */
+	@WebMethod
+	User updateUser(String userId, String password, User user) throws UsersException;
+	
+	/**
+	 * Deletes the user identified by userId. The spreadsheets owned by the user should be eventually removed (asynchronous
+	 * deletion is ok).
+	 * @param nauserId the userId of the user
+	 * @param password password of the user
+	 * @throws UsersException otherwise
+	 */
+	@WebMethod
+	User deleteUser(String userId, String password) throws UsersException;
+	
+	/**
+	 * Returns the list of users for which the pattern is a substring of the name (of the user), case-insensitive.
+	 * The password of the users returned by the query must be set to the empty string "".
+	 * @param pattern substring to search
+	 * @throws UsersException otherwise
+	 */
+	@WebMethod
+	List<User> searchUsers(String pattern) throws UsersException;
+}
